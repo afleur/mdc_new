e657eb957ea6cb8c26c523027e5bf9f9221f59d5
Alois Fleuricourt <afleuricourt@gmail.com>
2020-07-30 16:09:03 +0200

    js home

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,8 +1,2 @@
 //= require rails-ujs
 //= require_tree .
-import { initUpdateNavbarOnScroll } from '../components/navbar';
-
-document.addEventListener('turbolinks:load', () => {
-  // Call your JS functions here
-  initUpdateNavbarOnScroll();
-});
--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,1 +1,10 @@
 import "bootstrap";
+
+import { initUpdateNavbarOnScroll } from '../plugins/navbar';
+
+document.addEventListener('turbolinks:load', () => {
+  // Call your JS functions here
+  initUpdateNavbarOnScroll();
+});
+
+import { go } from '../plugins/hometitle';
