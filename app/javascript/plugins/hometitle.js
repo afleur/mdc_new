66292e3db18c964a3781a87ea28886dce9e84457
Alois Fleuricourt <afleuricourt@gmail.com>
2020-08-23 22:27:44 +0200

    fixing heroku pb

--- app/javascript/plugins/hometitle.js
+++ app/javascript/plugins/hometitle.js
@@ -1,4 +1,4 @@
-//function changeWord (){
+function changeWord (){
     const entrepreneure = document.querySelector(".entrepreneure")
     const artiste = document.querySelector(".artiste");
     const porteuse = document.querySelector(".porteuse");
@@ -6,34 +6,34 @@
     const dirigeante = document.querySelector(".dirigeante");
     const militante = document.querySelector(".militante");
 
-    //if (artiste.classList.contains("no-display")) {
-      //artiste.style.display = "";
-      //artiste.classList.remove("no-display");
-      //entrepreneure.style.display = "none";
-    //} else if (porteuse.classList.contains("no-display")) {
-      //porteuse.style.display = "";
-      //porteuse.classList.remove("no-display");
-      //artiste.style.display = "none";
-    //} else if (sportive.classList.contains("no-display")){
-      //sportive.style.display = "";
-      //sportive.classList.remove("no-display");
-      //porteuse.style.display = "none";
-    //} else if (dirigeante.classList.contains("no-display")){
-      //dirigeante.style.display = "";
-      //dirigeante.classList.remove("no-display");
-      //sportive.style.display = "none";
-    //} else if (militante.classList.contains("no-display")){
-      //militante.style.display = "";
-      //militante.classList.remove("no-display");
-      //dirigeante.style.display = "none";
-    //} else {
-      //entrepreneure.style.display = "";
-      //militante.style.display = "none";
-      //}
+    if (artiste.classList.contains("no-display")) {
+      artiste.style.display = "";
+      artiste.classList.remove("no-display");
+      entrepreneure.style.display = "none";
+    } else if (porteuse.classList.contains("no-display")) {
+      porteuse.style.display = "";
+      porteuse.classList.remove("no-display");
+      artiste.style.display = "none";
+    } else if (sportive.classList.contains("no-display")){
+      sportive.style.display = "";
+      sportive.classList.remove("no-display");
+      porteuse.style.display = "none";
+    } else if (dirigeante.classList.contains("no-display")){
+      dirigeante.style.display = "";
+      dirigeante.classList.remove("no-display");
+      sportive.style.display = "none";
+    } else if (militante.classList.contains("no-display")){
+      militante.style.display = "";
+      militante.classList.remove("no-display");
+      dirigeante.style.display = "none";
+    } else {
+      entrepreneure.style.display = "";
+      militante.style.display = "none";
+      }
 
-//}
+}
 
-//const go = setInterval(changeWord, 3000);
+const go = setInterval(changeWord, 3000);
 
 
-//export { go };
+export { go };
